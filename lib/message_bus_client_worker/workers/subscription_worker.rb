b3656d4c6692c408f5317c7cd8086b8d33c5fc70
Ramon Tayag <ramon.tayag@gmail.com>
2018-08-03 09:14:34 +0800

    Resume messages where last left off

--- lib/message_bus_client_worker/workers/subscription_worker.rb
+++ lib/message_bus_client_worker/workers/subscription_worker.rb
@@ -4,22 +4,49 @@ module MessageBusClientWorker
     include Sidekiq::Worker
     sidekiq_options retry: false
 
+    CHANNEL_INDICES_REDIS_HASH_NAME = "message_bus_client_worker_channel_indices"
+
     def perform(host, channel, processor_class_name, long=true)
       client_id = SecureRandom.uuid
       uri = Addressable::URI.parse(host)
       uri.path = "/message-bus/#{client_id}/poll"
 
       params = {dlp: "t"}
-      form_params = {channel => 0}
+      form_params = {
+        channel => get_last_id_from_redis(host: host, channel: channel)
+      }
       body = HTTP.post(uri.to_s, params: params, form: form_params).body
 
       messages = JSON.parse(body.to_s)
       processor_class = Kernel.const_get(processor_class_name)
 
       messages.each do |message|
+        set_last_id_in_redis(host: host, channel: channel, message: message)
         processor_class.(message["data"])
       end
     end
 
+    def set_last_id_in_redis(host:, channel:, message:)
+      hash_key = "#{host}-#{channel}"
+      message_id = message["message_id"]
+
+      Sidekiq.redis do |r|
+        r.hset(CHANNEL_INDICES_REDIS_HASH_NAME, hash_key, message_id)
+      end
+    end
+
+    def get_last_id_from_redis(host:, channel:)
+      hash_key = "#{host}-#{channel}"
+
+      id = Sidekiq.redis do |r|
+        r.hget(CHANNEL_INDICES_REDIS_HASH_NAME, hash_key)
+      end
+
+      if id.to_i == -1
+        '0'
+      else
+        id
+      end
+    end
   end
 end
--- spec/lib/message_bus_client_worker/workers/subscription_worker_spec.rb
+++ spec/lib/message_bus_client_worker/workers/subscription_worker_spec.rb
@@ -29,7 +29,41 @@ module MessageBusClientWorker
       it "makes a call with polling set to true, attempting to stream"
     end
 
-    it "continues where it left off"
+    context "recovery" do
+      let!(:second_processor) do
+        SaverProcessor = Class.new do
+          def self.call(payload)
+            REDIS.set(payload["name"], payload["data"])
+          end
+        end
+      end
+
+      it "continues where it left off" do
+        name_randomness = SecureRandom.uuid
+        message_1 = SecureRandom.uuid
+        Publish.("First #{name_randomness}", message_1)
+
+        described_class.new.perform(
+          CONFIG[:chat_server_url],
+          "/message",
+          TestProc.to_s,
+          false,
+        )
+
+        message_2 = SecureRandom.uuid
+        Publish.("Second #{name_randomness}", message_2)
+
+        described_class.new.perform(
+          CONFIG[:chat_server_url],
+          "/message",
+          SaverProcessor.to_s,
+          false,
+        )
+
+        expect(REDIS.get("First #{name_randomness}")).to be_nil
+        expect(REDIS.get("Second #{name_randomness}")).to eq message_2
+      end
+    end
 
   end
 end
