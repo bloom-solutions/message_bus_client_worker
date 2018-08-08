c23b89a2583f3884ead960dcf3f47f72d5d860ec
Ramon Tayag <ramon.tayag@gmail.com>
2018-08-08 08:12:34 +0800

    message_id of a channel is one of these in priority:
    - last recorded message id
    - configured message id
    - "-1"

--- lib/message_bus_client_worker/services/polling/generate_params.rb
+++ lib/message_bus_client_worker/services/polling/generate_params.rb
@@ -6,11 +6,15 @@ module MessageBusClientWorker
       expects :host, :subscriptions
       promises :params, :form_params
 
+      DEFAULT_MESSAGE_ID = "-1".freeze
+
       executed do |c|
         c.params = { dlp: 't' }
         c.form_params = c.subscriptions.each_with_object({}) do |sub, hash|
           custom_message_id = sub[1][:message_id] ? sub[1][:message_id].to_s : nil
-          hash[sub[0]] = custom_message_id || GetLastId.(c.host, sub[0])
+          hash[sub[0]] = GetLastId.(c.host, sub[0]) ||
+            custom_message_id ||
+            DEFAULT_MESSAGE_ID
         end
       end
 
--- spec/lib/message_bus_client_worker/services/polling/generate_params_spec.rb
+++ spec/lib/message_bus_client_worker/services/polling/generate_params_spec.rb
@@ -4,19 +4,30 @@ module MessageBusClientWorker
   module Polling
     RSpec.describe GenerateParams do
 
-      it "generates the channel indices with from the last message that was processed, if any" do
+      it([
+        "sets form_params with channel indices in priority:",
+        "last recorded id, configured custom id, and finally -1",
+      ].join) do
         SetLastId.("https://host.com", "/points", 20)
 
         resulting_ctx = described_class.execute(
           host: "https://host.com",
           subscriptions: {
             "/messages" => { processor: "DoesNotMatterInThisSpec" },
-            "/points" => { processor: "DoesNotMatterInThisSpec" },
+            "/points" => {
+              processor: "DoesNotMatterInThisSpec",
+              message_id: 15,
+            },
+            "/read-before" => {
+              processor: "DoesNotMatterInThisSpec",
+              message_id: 1,
+            },
           }
         )
 
-        expect(resulting_ctx.form_params["/messages"]).to eq "0"
+        expect(resulting_ctx.form_params["/messages"]).to eq "-1"
         expect(resulting_ctx.form_params["/points"]).to eq "20"
+        expect(resulting_ctx.form_params["/read-before"]).to eq "1"
       end
 
     end
