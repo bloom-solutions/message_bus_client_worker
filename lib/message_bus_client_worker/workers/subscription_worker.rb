8901c4e2e236f92f6c02e4aaf58afe4f70ba68cd
Ramon Tayag <ramon.tayag@gmail.com>
2018-09-20 11:23:52 +0800

    test

--- lib/message_bus_client_worker/workers/subscription_worker.rb
+++ lib/message_bus_client_worker/workers/subscription_worker.rb
@@ -4,18 +4,18 @@ module MessageBusClientWorker
     include Sidekiq::Worker
     sidekiq_options(
       retry: false,
-      lock: :until_executed,
-      unique_args: :unique_args,
-      on_conflict: :log,
+      # lock: :until_executed,
+      # unique_args: :unique_args,
+      # on_conflict: :log,
     )
 
     def perform(host, subscriptions, long=false)
       Poll.(host, subscriptions.with_indifferent_access, long)
     end
 
-    def self.unique_args(args)
-      args
-    end
+    # def self.unique_args(args)
+    #   args
+    # end
 
   end
 end
