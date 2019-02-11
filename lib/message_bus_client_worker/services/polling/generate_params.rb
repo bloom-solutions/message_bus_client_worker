49b75c931797e19187d08a436c6b51e31ba27d60
Mark Chavez <markchav3z@gmail.com>
2019-02-11 16:36:19 +0800

    GenerateParams uses channels to get subscriptions

--- lib/message_bus_client_worker/services/polling/generate_params.rb
+++ lib/message_bus_client_worker/services/polling/generate_params.rb
@@ -9,8 +9,10 @@ module MessageBusClientWorker
       DEFAULT_MESSAGE_ID = "-1".freeze
 
       executed do |c|
+        channels = c.subscriptions[:channels]
+
         c.params = { dlp: 't' }
-        c.form_params = c.subscriptions.each_with_object({}) do |sub, hash|
+        c.form_params = channels.each_with_object({}) do |sub, hash|
           custom_message_id = sub[1][:message_id] ? sub[1][:message_id].to_s : nil
           hash[sub[0]] = GetLastId.(c.host, sub[0]) ||
             custom_message_id ||
